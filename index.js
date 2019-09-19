9bbc71b4eee8a9cb61a78d5f16a5281875e14bb5
Tom C <tomc@3gmagic.com>
2019-09-19 16:22:08 +0000

    added test data

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 console.log("SP var");
 
 console.log("Load data");
-
+var d = require("./monthly_json");
 
 console.log("Caculate var");
 
 
-console.log("Show results");
+var i = 0;
+
+for (var c = 0; c < d[c]; c++) {
+
+    i++
+
+}
+
+
+console.log("Show results ", i, d.length);
 
